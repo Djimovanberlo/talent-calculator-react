3ae96d0fb64eb658f06cd3db66514ada6d8b594d
Djimovanberlo <djimovanberlo@gmail.com>
2020-10-23 12:12:59 +0200

    made classNav with icons (no functionality)

--- src/components/ClassesNav.jsx
+++ src/components/ClassesNav.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "../styles/classesNav.css"
+
+import druid from "../data/druid/images/druid.jpg"
+import hunter from "../data/hunter/images/hunter.jpg"
+import mage from "../data/mage/images/mage.jpg"
+import paladin from "../data/paladin/images/paladin.jpg"
+import priest from "../data/priest/images/priest.jpg"
+import rogue from "../data/rogue/images/rogue.jpg"
+import shaman from "../data/shaman/images/shaman.jpg"
+import warlock from "../data/warlock/images/warlock.jpg"
+import warrior from "../data/warrior/images/warrior.jpg"
 
 const ClassesNav = () => {
-    return <>ClassesNav Here</>
+    const iconArray = [druid, hunter, mage, paladin, priest, rogue, shaman, warlock, warrior]
+
+return <div id="tc-classes"><div id="tc-classes-outer" style={{display: "block"}}><div id="tc-classes-inner">{iconArray.map((classIcon) => {
+    return <img className="iconMedium" src={classIcon} />
+})}</div></div></div>
 }
 
 export default ClassesNav
+
+// <div className="iconMedium"><img src={druid}/></div>
